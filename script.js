337c23c15505c0f615bed1c948d1142a89eaafa2
RavirajMhalsekar <ravirajmhalsekar@gmail.com>
2021-08-11 18:38:28 +0530

    swipper added

--- script.js
+++ script.js
@@ -11,3 +11,23 @@ menu.addEventListener('click', () =>{
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
 });
+
+var swiper = new Swiper(".review-slider", {
+    spaceBetween: 20,
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnIntreaction: false,
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
+});
